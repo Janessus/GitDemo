48efdc054f98a40a469b0d89e2e6842298fa6622
Janes Heuberger <janes.heuberger@st-benz.de>
2023-02-07 19:31:19 +0100

    commiting very advanced feature

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ print("2nd message!")
 print("Complex is noob")       
 
 i = 0
+count = 0
 
 while(True):
+
     print("something " + i)
     if(i == 5):
         break
     i += 1
+    count += 1
 
 print("Done!")
