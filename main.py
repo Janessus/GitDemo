5dbd89d696839b80fbcfd08070146023343f7f9e
Janes Heuberger <janes.heuberger@st-benz.de>
2023-02-07 19:25:08 +0100

    dev changed

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 print("Hello NX!")
 print("2nd message!")
 
-print("Complex is noob")        
+print("Complex is noob")       
+
+i = 0
+
+while(True):
+    print("something " + i)
+    if(i == 5):
+        break
+    i += 1
+
+print("Done!")
